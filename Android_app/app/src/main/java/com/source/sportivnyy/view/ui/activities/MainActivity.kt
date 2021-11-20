5e9dfbb6b1b03039f236cb1078bd6792f8b4ca05
sfelipehh <sfelipehh@gmail.com>
2021-11-20 18:22:41 -0500

    Orden carpetas y ajuste de icono

--- Android_app/app/src/main/java/com/source/sportivnyy/view/ui/activities/MainActivity.kt
+++ Android_app/app/src/main/java/com/source/sportivnyy/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.source.sportivnyy
+package com.source.sportivnyy.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.source.sportivnyy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
