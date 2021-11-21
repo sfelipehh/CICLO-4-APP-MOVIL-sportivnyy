5fe4d8e42a3cf513923080c2535b9eb9f268c8e3
sfelipehh <sfelipehh@gmail.com>
2021-11-20 21:21:03 -0500

    Actualizacion splashscreen activity

--- Android_app/app/src/main/java/com/source/sportivnyy/view/ui/activities/SplashScreen.kt
+++ Android_app/app/src/main/java/com/source/sportivnyy/view/ui/activities/SplashScreen.kt
@@ -2,10 +2,15 @@ package com.source.sportivnyy.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.source.sportivnyy.R
+import com.source.sportivnyy.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
